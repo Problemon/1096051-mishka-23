a08fa7655a3e8b195bc74f670008f8dcdd5eebb5
Problemon <danilets.pasha@mail.ru>
2021-07-23 16:43:11 +1000

    Разметка по БЭМ все красиво классно и вкусно почти)

---
+++ source/js/map.js
@@ -0,0 +1,18 @@
+ymaps.ready(init);
+function init() {
+  let myMap = new ymaps.Map("map", {
+
+    center: [59.938635, 30.323118],
+
+    zoom: 18,
+  });
+
+  let myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/map/marker.svg',
+    iconImageSize: [67, 100],
+    iconImageOffset: [-33.5, -100]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
