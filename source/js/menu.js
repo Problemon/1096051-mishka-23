00bece53a43cc6a592b3b7446fdf983a76d245c2
Problemon <danilets.pasha@mail.ru>
2021-08-23 09:40:13 +1000

    Исправления

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,36 +1,35 @@
 const menuButton = document.querySelector(".navigation__button");
-const menuButtonStick = document.querySelector(".navigation__button-stick"); // визуальный вид кнопки
 const menu = document.querySelector(".navigation__menu");
 const userLinks = document.querySelectorAll(".user-list__link");
 
 menuButton.classList.remove("navigation__button--nojs"); // Если js не загрузился.
-menuButtonStick.classList.add("navigation__button-stick--closed");
+menuButton.classList.add("navigation__button--closed");
 swapClass(menu, "navigation__menu--nojs", "navigation__menu--hidden")
 
 menuButton.addEventListener("click", function() {
-  toggle(menuButtonStick, "navigation__button-stick--opened", "navigation__button-stick--closed");
+  toggle(menuButton, "navigation__button--opened", "navigation__button--closed");
   toggle(menu, "navigation__menu--hidden", "navigation__menu--visible")
 });
 
 // Клик мимо меню, закрывает его
 window.addEventListener("click", function(evt) {
   if (!evt.target.closest(".navigation__button") && !evt.target.closest(".navigation__menu--visible")) {
-      swapClass(menu, "navigation__menu--visible", "navigation__menu--hidden");
-      swapClass(menuButtonStick, "navigation__button-stick--opened", "navigation__button-stick--closed");
+    swapClass(menu, "navigation__menu--visible", "navigation__menu--hidden");
+    swapClass(menuButton, "navigation__button--opened", "navigation__button--closed");
   };
 });
 
 // Чтобы фокус не сбежал)
 userLinks[userLinks.length - 1].onfocus = function () {
-    function focusTrap (evt) {
-        if(evt.key === "Tab") {
-            evt.preventDefault();
-            menuButton.focus();
+  function focusTrap (evt) {
+    if(evt.key === "Tab") {
+      evt.preventDefault();
+      menuButton.focus();
 
-            window.removeEventListener("keydown", focusTrap);
-        }
+      window.removeEventListener("keydown", focusTrap);
     }
-    window.addEventListener("keydown", focusTrap);
+  }
+  window.addEventListener("keydown", focusTrap);
 };
 
 function toggle (element, on, off) {
