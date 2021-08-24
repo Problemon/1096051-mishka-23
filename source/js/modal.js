d5711d303f2c647ec06baa3ceef314969d499ba2
Problemon <danilets.pasha@mail.ru>
2021-08-25 07:49:30 +1000

    Функционирвоание модального окна и исправление отступов у социалок

---
+++ source/js/modal.js
@@ -0,0 +1,49 @@
+const modalBasket = document.querySelector(".modal-basket");
+const buttonOrder = document.querySelectorAll(".button-order");
+const buttonsSize = document.querySelectorAll(".modal-basket__radio");
+const buttonAdd   = document.querySelector(".modal-basket__button");
+
+let visible = "modal-basket--visible";
+let hidden  = "modal-basket--hidden";
+
+buttonOrder.forEach( el => {
+  el.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    swap(modalBasket, visible, hidden);
+    buttonsSize[0].focus();
+  });
+});
+
+window.addEventListener("click", (evt) => {
+  if (modalBasket.classList.contains(visible)) {
+    if (!evt.target.closest(".modal-basket__container") && !evt.target.closest(".button-order")) {
+      swap(modalBasket, hidden, visible);
+    };
+  };
+});
+
+window.addEventListener("keydown", (evt) => {
+  if (modalBasket.classList.contains(visible)) {
+    if (evt.key === "Esc" || evt.key === "Escape") {
+      swap(modalBasket, hidden, visible);
+    }
+  };
+});
+
+buttonAdd.onfocus = () => {
+  function focusTrap (evt) {
+    if(evt.key === "Tab") {
+      evt.preventDefault();
+      buttonsSize[0].focus();
+
+      window.removeEventListener("keydown", focusTrap);
+    };
+  };
+  window.addEventListener("keydown", focusTrap);
+};
+
+
+function swap (element, on, off) {
+  element.classList.toggle(on);
+  element.classList.toggle(off);
+}
